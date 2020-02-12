64d69c6068050522e774cd0b3127b128d6854632
Dmitri <dmitri.murphy@gmail.com>
2020-02-12 01:01:25 -0800

    Initial Commit

---
+++ resources/views/mainpage.blade.php
@@ -0,0 +1,17 @@
+@extends("layouts.master")
+
+@section('body')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Water Reuse App!</div>
+
+                    <div class="card-body">
+                        Main page...
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
