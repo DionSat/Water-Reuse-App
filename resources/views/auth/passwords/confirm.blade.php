653b56759927b782f599c43d52a82c2690a8b211
Dmitri <dmitri.murphy@gmail.com>
2020-02-12 15:57:13 -0800

    Fix authentication pages

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
-@section('content')
+@section('body')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
