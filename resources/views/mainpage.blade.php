026f36605fca21ae6b3a6dafc3c6355e6e76c491
Dmitri <dmitri.murphy@gmail.com>
2020-04-14 18:22:24 -0700

    Improve homepage with picture background

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -1,25 +1,41 @@
 @extends("layouts.master")
 
 @section('body')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Water Reuse App!</div>
-
-                    <div class="card-body">
-                        Main page...
-
-                        <br>
-                        <a href="{{route("search")}}"> Go to the Search Page!</a>
-                        <br>
-                        <br>
-                        Note that this one will ask you to log in first, then take you to the page:
-                        <br>
-                        <a href="{{route("home")}}"> Go to the Dashboard/ Logged-in Homepage!</a>
-                    </div>
+    <div class="container main-content mt-5">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="">
+                    <h1 class="main-title text-muted-red roboto"> Water Reuse Application</h1>
+                    <h4 class="main-subtext mplus ml-1"> Helping make re-using water easier since 2020. </h4>
                 </div>
+
             </div>
         </div>
     </div>
 @endsection
+
+@push("css")
+<style>
+    html, body {
+        height: 125%;
+    }
+    body {
+        background: url("img/adrien-olichon-unsplash.jpg");
+        background-size: cover;
+    }
+    .main-content .card {
+        padding: 35px;
+        opacity: 0.87;
+        border-radius: 25px;
+    }
+
+    .main-title {
+        -webkit-text-stroke: 2px black;
+        font-weight: 800;
+        font-size: 5.5rem;
+        color: white;
+    }
+</style>
+
+
+@endpush
