180d63964ad731206f28d7d2b24bb9052b8208e0
Dmitri <dmitri.murphy@gmail.com>
2020-04-16 01:58:38 -0700

    Center the title

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -3,9 +3,9 @@
 @section('body')
     <div class="container main-content mt-5">
         <div class="row">
-            <div class="col-md-6">
-                <div class="">
-                    <h1 class="main-title text-muted-red roboto"> Water Reuse Application</h1>
+            <div class="col-md-12">
+                <div class="heading-card text-center">
+                    <h1 class="main-title text-muted-red roboto"> Water Reuse Directory</h1>
                     <h4 class="main-subtext mplus ml-1"> Helping make re-using water easier since 2020. </h4>
                 </div>
 
@@ -29,12 +29,22 @@
         border-radius: 25px;
     }
 
+    .heading-card{
+        background: rgba(255, 255, 255, 0.85);
+        padding: 20px;
+        border-radius: 10px;
+    }
+
     .main-title {
-        -webkit-text-stroke: 2px black;
+        opacity: 1;
         font-weight: 800;
         font-size: 5.5rem;
-        color: white;
+        /*color: #2e909e;*/
+        color: #22707b;
+
     }
+
+
 </style>
 
 
