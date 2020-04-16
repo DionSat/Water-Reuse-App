81c9060601dfb98ee5da95e4744207aa9151348e
giang <giang@pdx.edu>
2020-04-16 00:56:14 -0700

    added new columns to links table (migrations)

---
+++ database/migrations/2020_04_16_071236_update_links.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateLinks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->string('name')->nullable();
+            $table->enum('status', ['valid', 'broken', 'unknown'])->nullable();
+            $table->dateTime('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('links', function (Blueprint $table) {
+            $table->dropColumn(['name', 'status', 'updated_at']);
+        });
+    }
+}
