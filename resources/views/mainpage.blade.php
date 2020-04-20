e961d88d559b8f9e8b75ffb85626f4e5d2f004a8
Dmitri <dmitri.murphy@gmail.com>
2020-04-20 16:34:25 -0700

    Temp work

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -8,19 +8,43 @@
                     <h1 class="main-title text-muted-red roboto"> Water Reuse Directory</h1>
                     <h4 class="main-subtext mplus ml-1"> Helping make re-using water easier since 2020. </h4>
                 </div>
-
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div id="mapid"></div>
             </div>
         </div>
     </div>
 @endsection
 
+@push("js")
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
+            integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
+            crossorigin=""></script>
+    <script>
+        var map = L.map('mapid').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker([51.5, -0.09]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
+    </script>
+@endpush
+
 @push("css")
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+          integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+          crossorigin=""/>
 <style>
     html, body {
         height: 125%;
     }
     body {
-        background: url("img/adrien-olichon-unsplash.jpg");
+        background: url("img/bird-s-eye-view-of-ocean-during-daytime-2707756.jpg");
         background-size: cover;
     }
     .main-content .card {
@@ -41,9 +65,12 @@
         font-size: 5.5rem;
         /*color: #2e909e;*/
         color: #22707b;
-
     }
 
+    #mapid{
+        height: 300px;
+        width: 500px;
+    }
 
 </style>
 
