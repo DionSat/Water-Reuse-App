fb92d0f8dde5a13561563da177a235c1cda8fd44
Dmitri <dmitri.murphy@gmail.com>
2020-04-23 18:27:51 -0700

    Working

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -9,38 +9,63 @@
                     <h1 class="display-4 main-title">Water Reuse Directory</h1>
                     <p class="lead"> A database of crowdsourced and verified re-use regulations. </p>
                     <hr class="my-4">
-                    <p>In a time where access to usable water is becoming more scarce, enabling people to re-using water properly is a important step to take.</p>
-                    {{--<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>--}}
-                </div>
-                <div class="heading-card text-center">
-                    <h1 class="main-title text-muted-red roboto"> Water Reuse Directory</h1>
-                    <h4 class="main-subtext mplus ml-1"> Helping make re-using water easier since 2020. </h4>
+                    <p>In a time where access to usable water is becoming more scarce, enabling people to re-use water properly is a important step to take.</p>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div id="mapid"></div>
+        <div class="row justify-content-center">
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 @endsection
 
 @push("js")
-    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-            integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-            crossorigin=""></script>
-    <script>
-        var map = L.map('mapid').setView([51.505, -0.09], 13);
+    {{--<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"--}}
+            {{--integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="--}}
+            {{--crossorigin=""></script>--}}
+    {{--<script>--}}
+        {{--var map = L.map('mapid').setView([51.505, -0.09], 13);--}}
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        {{--L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {--}}
+            {{--attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'--}}
+        {{--}).addTo(map);--}}
 
-        L.marker([51.5, -0.09]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
-    </script>
+        {{--L.marker([51.5, -0.09]).addTo(map)--}}
+            {{--.bindPopup('A pretty CSS3 popup.<br> Easily customizable.')--}}
+            {{--.openPopup();--}}
+    {{--</script>--}}
 @endpush
 
 @push("css")
@@ -55,30 +80,30 @@
         background: url("img/bird-s-eye-view-of-ocean-during-daytime-2707756.jpg");
         background-size: cover;
     }
-    .main-content .card {
-        padding: 35px;
-        opacity: 0.87;
-        border-radius: 25px;
-    }
+    /*.main-content .card {*/
+        /*padding: 35px;*/
+        /*opacity: 0.87;*/
+        /*border-radius: 25px;*/
+    /*}*/
 
-    .heading-card{
-        background: rgba(255, 255, 255, 0.85);
-        padding: 20px;
-        border-radius: 10px;
-    }
+    /*.heading-card{*/
+        /*background: rgba(255, 255, 255, 0.85);*/
+        /*padding: 20px;*/
+        /*border-radius: 10px;*/
+    /*}*/
 
-    .main-title {
-        opacity: 1;
-        font-weight: 800;
-        font-size: 5.5rem;
-        /*color: #2e909e;*/
-        color: #22707b;
-    }
+    /*.main-title {*/
+        /*opacity: 1;*/
+        /*font-weight: 800;*/
+        /*font-size: 5.5rem;*/
+        /*!*color: #2e909e;*!*/
+        /*color: #22707b;*/
+    /*}*/
 
-    #mapid{
-        height: 300px;
-        width: 500px;
-    }
+    /*#mapid{*/
+        /*height: 300px;*/
+        /*width: 500px;*/
+    /*}*/
 
 </style>
 
