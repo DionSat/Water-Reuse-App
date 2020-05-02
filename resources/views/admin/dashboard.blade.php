282fc9633568b5ed71a4085faa18e87f9ac35b3b
Tyler Race <tylerrrace@gmail.com>
2020-05-01 19:44:14 -0700

    Fixed formatting of page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -16,30 +16,32 @@
         </div>
         </br>
 
-        <div class="row justify-content-center" id="statsPage">
-            @foreach($userAndEmail as $card)
-                <div class="col-md-3">
-                    <div class="card h-80 shadow">
-                        <div class="card-body">
-                            <div class="d-flex">
-                                <div class="p-2 mr-3 w-75">
-                                    <h4 class="card-title">{{$card["title"]}}</h4>
-                                </div>
-                                <div>
-                                    <h1 class="m-3 flex-grow-1 w-100 text-muted"> {{$card["count"]}}</h1>
+        <div class="container" id="statsPage">
+            <div class="row justify-content-center">
+                @foreach($userAndEmail as $card)
+                    <div class="col-md-3">
+                        <div class="card h-80 shadow">
+                            <div class="card-body">
+                                <div class="d-flex">
+                                    <div class="p-2 mr-3 w-75">
+                                        <h4 class="card-title">{{$card["title"]}}</h4>
+                                    </div>
+                                    <div>
+                                        <h1 class="m-3 flex-grow-1 w-100 text-muted"> {{$card["count"]}}</h1>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="card-footer">
-                            <div class="form-row mt-3 justify-content-center">
-                                <div class="col-md-6">
-                                    <a href="{{$card["view"]}}" class="btn btn-primary d-block"> <i class="fas fa-eye"></i> View </a>
+                            <div class="card-footer">
+                                <div class="form-row mt-3 justify-content-center">
+                                    <div class="col-md-6">
+                                        <a href="{{$card["view"]}}" class="btn btn-primary d-block"> <i class="fas fa-eye"></i> View </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
 
         <div class="container" id="dbStatsPage">
@@ -166,7 +168,7 @@
     function onLoad() {
         var stats = document.getElementById("statsPage");
         var DBstats = document.getElementById("dbStatsPage");
-        stats.style.display = "inline";
+        stats.style.display = "block";
         DBstats.style.display = "none";
     }
 </script>
