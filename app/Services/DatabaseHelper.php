8c5619c58912d03e0c1158dd324f930817b104b3
Tyler Race <tylerrrace@gmail.com>
2020-05-11 16:14:11 -0700

    Started helper class. Implemented try/catch in state save

---
+++ app/Services/DatabaseHelper.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+
+class DatabaseHelper {
+    // Add pending state merge
+    public static function addPendingStateMerge(Request $request) {
+
+    }
+    // Remove pending state merge
+    public static function deletePendingStateMerge(Request $request) {
+
+    }
+
+    // Add pending county merge
+    public static function addPendingCountyMerge(Request $request) {
+
+    }
+    // Remove pending county merge
+    public static function deletePendingCountyMerge(Request $request) {
+
+    }
+
+    // Add pending city merge
+
+    // Remove pending city merge
+
+
+    // Add state merge
+
+    // Remove state merge
+
+
+    // Add county merge
+
+    // Remove county merge
+
+
+    // Add city merge
+
+    // Remove city merge
+
+
+    // Move merge from pending to approved merge table
+
+}
