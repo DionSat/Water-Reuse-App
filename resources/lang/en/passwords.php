339cca090a1f988ecc02d8db46d205d345c94eb4
Dmitri <dmitri.murphy@gmail.com>
2020-05-20 01:18:34 -0700

    Update password reset message

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -14,7 +14,7 @@ return [
     */
 
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
+    'sent' => "We have e-mailed your password reset link! Please check your spam folder if you don't see it in your inbox.",
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
