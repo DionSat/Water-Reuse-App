8c84cda626195727cbdde436dfe2864bdcadc294
giang <giang@pdx.edu>
2020-05-26 19:25:28 -0700

    added an approve button when viewing contributing submissions

--- resources/views/common/generic-reuse-item.blade.php
+++ resources/views/common/generic-reuse-item.blade.php
@@ -12,10 +12,24 @@
                     @if(($item->getStatus() != "approved" && $item->user_id === Auth::user()->id) || Auth::user()->is_admin)
                         <div class="my-3 row d-flex justify-content-between">
                             <span class="text-center col-md-4">
-                                <a href="{{route('submissionEdit', ["type" => $item->getLocationType(), "state" => $item->getStatus(), "itemId" => $item->id, "back" => url()->full(), "previousBack" => $backUrl])}}" class="btn btn-primary btn-block"> Edit </a>
+                                <a href="{{route('submissionEdit', ["type" => $item->getLocationType(), "state" => $item->getStatus(), "itemId" => $item->id, "back" => url()->full(), "previousBack" => $backUrl])}}"
+                                   class="btn btn-primary btn-block"> Edit </a>
                             </span>
                             <span class="text-center col-md-4">
-                            <button class="btn btn-danger btn-block" data-toggle="modal" data-target="#exampleModal" type="button">Delete</button>
+{{--                                <form method="POST" action="@if($type==="state") {{'addStateMergeSubmit'}} @elseif($type==="county")--}}
+                                {{--                                    {{'addCountyMergeSubmit'}} @else {{'addCityMergeSubmit'}}" @endif>--}}
+                                <form method="POST" @if($type==="state") action="{{route('addStateMergeSubmit')}}"
+                                      @elseif($type==="county") action="{{route('addCountyMergeSubmit')}}"
+                                      @else action="{{route('addCityMergeSubmit')}}"@endif>
+                                                                   {{ csrf_field() }}
+                                                             <input name="id"
+                                                                    value="{{$item->id}}" hidden>
+                                <button type="submit" class="btn btn-success btn-block">Approve</button>
+                                </form>
+                            </span>
+                            <span class="text-center col-md-4">
+                            <button class="btn btn-danger btn-block" data-toggle="modal" data-target="#exampleModal"
+                                    type="button">Delete</button>
                             </span>
                         </div>
                     @else
@@ -31,31 +45,31 @@
 
     <!-- Modal -->
     @auth
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modal-title">Delete Submission Confirmation</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    Are you sure you want to delete this submission?
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <form action="{{ route('deleteItem') }}" method="POST">
-                        {{ csrf_field() }}
-                        <input type="text" name="back" style="display: none;" value="{{$backUrl}}">
-                        <input type="text" name="type" style="display: none;" value="{{$type}}">
-                        <input type="text" name="state" style="display: none;" value="{{$item->getStatus()}}">
-                        <input type="number" name="id" style="display: none;" value="{{$item->id}}">
-                        <button type="submit" class="btn btn-danger">Confirm Delete</button>
-                    </form>
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modal-title">Delete Submission Confirmation</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Are you sure you want to delete this submission?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <form action="{{ route('deleteItem') }}" method="POST">
+                            {{ csrf_field() }}
+                            <input type="text" name="back" style="display: none;" value="{{$backUrl}}">
+                            <input type="text" name="type" style="display: none;" value="{{$type}}">
+                            <input type="text" name="state" style="display: none;" value="{{$item->getStatus()}}">
+                            <input type="number" name="id" style="display: none;" value="{{$item->id}}">
+                            <button type="submit" class="btn btn-danger">Confirm Delete</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     @endauth
 @endsection
