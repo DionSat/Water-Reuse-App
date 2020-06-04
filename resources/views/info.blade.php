2230e9914054e239207e3c07750206fb1795e452
Paul <phubbard@pdx.edu>
2020-06-04 14:07:55 -0700

    Finished base user and contributing user information. Now on the admin!

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -3,25 +3,38 @@
 @section('body')
     <div class="container" id="container">
         @guest
-        <div class="row justify-content-center" style="margin: 3em">
-            <div class="col-md-8">
-                <div class="container" id="guest">
-                    <h2>How to Search for a Regulation</h2>
-                    <p>1. Navigate to the <a href="{{ route('search') }}"> Search </a>page</p>
-                    <p>2. Select either 'Commercial' or 'Residential'</p>
-                    <img src="img/searchExample.png"></img>
-                    <p>3. First select a state in the dropdown menu, then you can either search only by state, or by county or city by selecting them in the dropdown menu.</p>
-                    <img src="img/searchExample1.png"></img>
-                    <p>4. After selecting the region you wish to search for, click on the 'Search' button.</p>
-                    <img src="img/searchExample2.png"></img>
-                    <p>5. At this point, you can either scroll through the search results, or narrow them down by selecting a source and destination in the dropdown menues.</p>
-                    <img src="img/searchExample3.png"></img>
-                    <h4>Disclaimer</h4>
-                    <p>If the state, county or city does not have any water reuse information, or if the state, county or city is not present in the search dropdown menus, this does not mean there are no regulations. It just means they have not been added to this site. Please search your local government web pages to find the water reuse information you need.</p>
+        <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <a class="nav-item nav-link active" id="nav-searchGuest-tab" data-toggle="tab" href="#nav-searchGuest" role="tab" aria-controls="nav-searchGuest" aria-selected="true">Search for a Regulation</a>
+                    <a class="nav-item nav-link" id="nav-register-tab" data-toggle="tab" href="#nav-register" role="tab" aria-controls="nav-register" aria-selected="false">Become a Contributor</a>
                 </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-searchGuest" role="tabpanel" aria-labelledby="nav-searchGuest-tab">
+                <h2>How to Search for a Regulation</h2>
+                <p>1. Navigate to the <a href="{{ route('search') }}" target="_blank"> Search </a>page</p>
+                <p>2. Select either 'Commercial' or 'Residential'</p>
+                <img src="img/searchExample.png"></img>
+                <p>3. First select a state in the dropdown menu, then you can either search only by state, or by county or city by selecting them in the dropdown menu.</p>
+                <img src="img/searchExample1.png"></img>
+                <p>4. After selecting the region you wish to search for, click on the 'Search' button.</p>
+                <img src="img/searchExample2.png"></img>
+                <p>5. At this point, you can either scroll through the search results, or narrow them down by selecting a source and destination in the dropdown menues.</p>
+                <img src="img/searchExample3.png"></img>
+                <h4>Disclaimer</h4>
+                <p>If the state, county or city does not have any water reuse information, or if the state, county or city is not present in the search dropdown menus, this does not mean there are no regulations. It just means they have not been added to this site. Please search your local government web pages to find the water reuse information you need.</p>
+            </div>
+            <div class="tab-pane fade" id="nav-register" role="tabpanel" aria-labelledby="nav-register-tab">
+                <h2>Register to Add Water Reuse Regulations</h2>
+                <p>1. Navigate to the <a href="{{ route('register') }}" target="_blank">'Register' </a>page</p>
+                <p>2. Enter in your name, a valid email address and a password.</p>
+                <img src="img/registerExample.png"></img>
+                <p>3. At this point, you can either click the 'Register' button, or you can add more information about yourself before you register. This information is very helpful to us, but it is not necessary.</p>
+                <img src="img/registerExample1.png"></img>
+                <p>3. After submitting your registration, you can add new water reuse regulations! Come back to the information page, as there will be new instructions on how to add a new regulation made available to you.</p>
             </div>
         </div>
-        @else
+        @elseif (Auth::check() && Auth::user()->is_admin)
             <nav>
                 <div class="nav nav-tabs" id="nav-tab" role="tablist">
                     <a class="nav-item nav-link active" id="nav-search-tab" data-toggle="tab" href="#nav-search" role="tab" aria-controls="nav-search" aria-selected="true">Search for a Regulation</a>
@@ -48,22 +61,27 @@
                 <h2>Add a New Regulation</h2>
                 <p>1. Navigate to the <a href="{{ route('userSubmission') }}" target="_blank">'Submit New Regulation' </a>page</p>
                 <p>2. At the very least, enter the State you wish to submit a regulation for. You can also select a County and City, but that is not a requirement.</p>
+                <img src="img/regSubmitExample.png"></img>
                 <p>3. You may also enter a new county or city by clicking the 'Add A New State, County or City' button, and entering the new county and / or city name.</p>
+                <img src="img/regSubmitExample1.png"></img>
                 <p>3. Select a source and a destination, and select the water reuse allowance (is allowed, not allowed, maybe allowed).</p>
-                <p>4. Add optional links to the water reuse regulation.</p>
+                <img class="smallRec" src="img/regSubmitExample2.png"></img>
+                <p>4. Add optional links to the water reuse regulation, and / or an optional comment about the regulation.</p>
+                <img class="smallRec" src="img/regSubmitExample3.png"></img>
                 <p>5. Either click "Submit" if you only have one source and destination, or click the "Add another regulation" button, and repeat steps 3 - 4.</p>
-
+                <img class="smallRec" src="img/regSubmitExample4.png"></img>
+                <p>6. After submiting your new regulation, you should see a success message. If you receive an error, please contact the administrator to solve the issue.</p>
+                <img  src="img/regSubmitExample5.png"></img>
             </div>
 
                 <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-                   <div class="row justify-content-center @if (Route::current()->getName() == "admin") active @endif"  style="margin: 3em">
                         <div class="container">
                             <ul>
                                 <li>
                                     <a href="#userManip">User Manipulation</a>
                                     <ul>
                                         <li>
-                                            <a href="#addUser">Add a New User</a>
+                                            <a href="#deleteUser">Delete a User</a>
                                         </li>
                                     </ul>
                                 </li>
@@ -76,14 +94,52 @@
                             </ul>
                             <hr id="userManip">
                             <h2>User Manipulation</h2>
-                            <h4 id="addUser" style="margin-top: 1em;">Add a New User</h4>
+                            <h4 id="deleteUser" style="margin-top: 1em;">Delete a User</h4>
                             <hr id="deleteReg">
                             <h2>Delete a Regulation</h2>
                             <hr id="deleteArea">
                             <h2>Delete a State, County or City</h2>
                         </div>
-                    </div>
                 </div>
+            </div>
+        @else
+        <nav>
+                <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                    <a class="nav-item nav-link active" id="nav-searchContrib-tab" data-toggle="tab" href="#nav-searchContrib" role="tab" aria-controls="nav-searchContrib" aria-selected="true">Search for a Regulation</a>
+                    <a class="nav-item nav-link" id="nav-addRegContrib-tab" data-toggle="tab" href="#nav-addRegContrib" role="tab" aria-controls="nav-addRegContrib" aria-selected="false">Add a New Regulation</a>
+                </div>
+            </nav>
+            <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-searchContrib" role="tabpanel" aria-labelledby="nav-search-tab">
+                <h2>How to Search for a Regulation</h2>
+                <p>1. Navigate to the <a href="{{ route('search') }}" target="_blank"> Search </a>page</p>
+                <p>2. Select either 'Commercial' or 'Residential'</p>
+                <img src="img/searchExample.png"></img>
+                <p>3. First select a state in the dropdown menu, then you can either search only by state, or by county or city by selecting them in the dropdown menu.</p>
+                <img src="img/searchExample1.png"></img>
+                <p>4. After selecting the region you wish to search for, click on the 'Search' button.</p>
+                <img src="img/searchExample2.png"></img>
+                <p>5. At this point, you can either scroll through the search results, or narrow them down by selecting a source and destination in the dropdown menues.</p>
+                <img src="img/searchExample3.png"></img>
+                <h4>Disclaimer</h4>
+                <p>If the state, county or city does not have any water reuse information, or if the state, county or city is not present in the search dropdown menus, this does not mean there are no regulations. It just means they have not been added to this site. Please search your local government web pages to find the water reuse information you need.</p>
+            </div>
+            <div class="tab-pane fade" id="nav-addRegContrib" role="tabpanel" aria-labelledby="nav-addRegContrib-tab">
+                <h2>Add a New Regulation</h2>
+                <p>1. Navigate to the <a href="{{ route('userSubmission') }}" target="_blank">'Submit New Regulation' </a>page</p>
+                <p>2. At the very least, enter the State you wish to submit a regulation for. You can also select a County and City, but that is not a requirement.</p>
+                <img src="img/regSubmitExample.png"></img>
+                <p>3. You may also enter a new county or city by clicking the 'Add A New State, County or City' button, and entering the new county and / or city name.</p>
+                <img src="img/regSubmitExample1.png"></img>
+                <p>3. Select a source and a destination, and select the water reuse allowance (is allowed, not allowed, maybe allowed).</p>
+                <img class="smallRec" src="img/regSubmitExample2.png"></img>
+                <p>4. Add optional links to the water reuse regulation, and / or an optional comment about the regulation.</p>
+                <img class="smallRec" src="img/regSubmitExample3.png"></img>
+                <p>5. Either click "Submit" if you only have one source and destination, or click the "Add another regulation" button, and repeat steps 3 - 4.</p>
+                <img class="smallRec" src="img/regSubmitExample4.png"></img>
+                <p>6. After submiting your new regulation, you should see a success message. If you receive an error, please contact the administrator to solve the issue.</p>
+                <img  src="img/regSubmitExample5.png"></img>
+            </div>
             </div>
         @endguest
 
@@ -101,15 +157,21 @@
             text-align: left;
         }
         img{
-            width: 485px;
-            height: 300px;
+            width: 647px;
+            height: 400px;
             border: 1px solid black !important;
         }
-        #nav-addReg, #nav-search, #nav-admin, #guest{
+        #nav-addReg, #nav-search, #nav-admin, #nav-searchGuest, #nav-register, #nav-addRegContrib, #nav-searchContrib{
             margin: 1.5em 0;
         }
-        #nav-addReg > *, #nav-search > *, #nav-admin > *, #guest > *{
+        #nav-addReg > *, #nav-search > *, #nav-admin > *, #nav-searchGuest > *, #nav-register > *, #nav-addRegContrib > *, #nav-searchContrib > *{
             margin: 1.5em 0;
+        }
+        .smallRec{
+            height: 200px;
+        }
+        #nav-contact{
+            margin: 2em 0;
         }
     </style>
 @endpush
