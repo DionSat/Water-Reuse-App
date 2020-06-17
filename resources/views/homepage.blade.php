93927e54ee96e436dee097e3712eb67f75737c18
Dmitri <dmitri.murphy@gmail.com>
2020-06-16 20:45:20 -0700

    Fix homepage mobile spacing

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -59,7 +59,7 @@
                 </div>
             </div>
             <div class="col-md-3">
-                <div class="card mt-3 mt-md-0 h-100">
+                <div class="card mt-4 mt-md-0 h-100">
                     <div class="card-body">
                         <div class="card-title text-center">
                             <h5 class="card-title">  A Recode Project  </h5>
