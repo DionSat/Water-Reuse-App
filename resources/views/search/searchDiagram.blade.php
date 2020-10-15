9b2bc81acbef834fbb48b1ab74e8d3b5de2bad9b
DionSat <43707506+DionSat@users.noreply.github.com>
2020-10-15 16:07:40 -0700

    There was a problem with the export fuctionality. A solution Liam presented to me was to turn the images into a base64 string then represent them at images. So I had add the functionality to convert images into base64 using convas. Then I had to add the functionality to wait for each image to load before converting them all and runnning the graph.

--- resources/views/search/searchDiagram.blade.php
+++ resources/views/search/searchDiagram.blade.php
@@ -2,6 +2,7 @@
 
 @section('body')
     <script src="{{ URL::asset('/libraries/orgchart.js') }}"></script>
+    <canvas id="myCanvas" width="578" height="200"></canvas>
     <div class="container">
         <div class="navbar">
             <a href="{{route("search")}}" class="btn btn-primary col-md-2 mb-4 float-left"> <i class="fas fa-arrow-circle-left"></i>
@@ -15,6 +16,61 @@
             <div class="card-body">
                 <div style="width:100%; height:700px;" id="orgchart"/>
                 <script>
+                    //Load all the icon images into an array
+                    var icons = new Array();
+                    icons[0] = new Image();
+                    icons[0].src = "{{ asset('img/water sources_1.jpg') }}";
+                    icons[1] = new Image();
+                    icons[1].src = "{{ URL::asset('img/app_KITCHEN SINK.jpg') }}";
+                    icons[2] = new Image();
+                    icons[2].src = "{{ URL::asset('img/app_KITCHEN SINK.jpg') }}";
+                    icons[3] = new Image();
+                    icons[3].src = "{{ URL::asset('img/app_DISHWASHER.jpg') }}";
+                    icons[4] = new Image();
+                    icons[4].src = "{{ URL::asset('img/app_LAVATORY.jpg') }}";
+                    icons[5] = new Image();
+                    icons[5].src = "{{ URL::asset('img/app_TUB-SHOWER.jpg') }}";
+                    icons[6] = new Image();
+                    icons[6].src = "{{ URL::asset('img/app_FIRE.jpg') }}";
+                    icons[7] = new Image();
+                    icons[7].src = "{{ URL::asset('img/app_CLOTHS WASHER.jpg') }}";
+                    icons[8] = new Image();
+                    icons[8].src = "{{ URL::asset('img/toilet.png') }}";
+                    icons[9] = new Image();
+                    icons[9].src = "{{ URL::asset('img/toilet.png') }}";
+                    icons[10] = new Image();
+                    icons[10].src = "{{ URL::asset('img/app_LAVATORY.jpg') }}";
+                    var imageCount = icons.length;
+                    var imagesLoaded = 0;
+
+                    //Use a for loop to load all the images one by one till all them are loaded
+                    //Then call the allLoaded function to execute your script
+                    for(var i=0; i<imageCount; i++){
+                        icons[i].onload = function(){
+                            imagesLoaded++;
+                            if(imagesLoaded == imageCount){
+                                allLoaded(icons, imageCount);
+                            }
+                        }
+                    }
+
+                    //This function runs when all the images are loaded and passes in the icons array and the number of icons
+                    function allLoaded(icons, imageCount){
+                        var string_icons = new Array();
+                        for(var i=0; i<imageCount; i++){
+                            var canvas = document.createElement('canvas'),
+                                ctx = canvas.getContext('2d');
+
+                            canvas.height = icons[i].naturalHeight;
+                            canvas.width = icons[i].naturalWidth;
+                            ctx.drawImage(icons[i], 0, 0);
+
+                            // Unfortunately, we cannot keep the original image type, so all images will be converted to PNG
+                            // For this reason, we cannot get the original Base64 string
+                            var uri = canvas.toDataURL('image/png'),
+                                b64 = uri.replace(/^data:image.+;base64,/, '');
+                            string_icons[i] = b64;
+                        }
 
                         OrgChart.toolbarUI.expandAllIcon = '<i  class="fas fa-layer-group fa-2x fa-fw" title="Expand Diagram"></i>';
                         OrgChart.toolbarUI.fitIcon = '<i class="fas fa-expand fa-2x fa-fw" title="Auto Fit Diagram"></i>';
@@ -22,53 +78,54 @@
                         OrgChart.toolbarUI.zoomInIcon = '<i class="fas fa-search-plus fa-2x fa-fw" title="Zoom In"></i>';
 
                         let chart = new OrgChart(document.getElementById("orgchart"), {
-                        template: "ana",
-                        enableSearch: false,
-                        menu: {
-                            pdf: {
-                                text: "Export PDF",
-                                icon: OrgChart.icon.pdf(24,24, '#7A7A7A'),
-                                onClick: preview
+                            template: "ana",
+                            enableSearch: false,
+                            menu: {
+                                pdf: {
+                                    text: "Export PDF",
+                                    icon: OrgChart.icon.pdf(24,24, '#7A7A7A'),
+                                    onClick: preview
+                                },
+                                png: { text: "Export PNG" },
+                                svg: { text: "Export SVG" },
+                                csv: { text: "Export CSV" }
                             },
-                            png: { text: "Export PNG" },
-                            svg: { text: "Export SVG" },
-                            csv: { text: "Export CSV" }
-                        },
-                        nodeMenu:{
-                            pdf: { text: "Export PDF" },
-                            png: { text: "Export PNG" },
-                            svg: { text: "Export SVG" },
-                            csv: { text: "Export CSV" }
-                        },
-                        toolbar: {
-                            zoom: true,
-                            fit: true,
-                            expandAll: true
-                        },
-                        nodeBinding: {
-                            field_0: "Name",
-                            img_0: "img"
-                        },
-                        nodes: [
-                            {id: 1, Name: "Condensate", img: "{{ URL::asset('img/water sources_1.jpg') }}"},
-                            {id: 2, pid: 1, Name: "Kitchen Sink", img: "{{ URL::asset('img/app_KITCHEN SINK.jpg') }}"},
-                            {id: 3, pid: 1, Name: "Kitchen Sink + Disposer", img: "{{ URL::asset('img/app_KITCHEN SINK.jpg') }}"},
-                            {id: 4, pid: 1, Name: "Dishwasher", img: "{{ URL::asset('img/app_DISHWASHER.jpg') }}"},
-                            {id: 5, pid: 1, Name: "Lavatory", img: "{{ URL::asset('img/app_LAVATORY.jpg') }}"},
-                            {id: 6, pid: 1, Name: "Tub + Shower", img: "{{ URL::asset('img/app_TUB-SHOWER.jpg') }}"},
-                            {id: 7, pid: 1, Name: "Fire Suppression", img: "{{ URL::asset('img/app_FIRE.jpg') }}"},
-                            {id: 8, pid: 1, Name: "Clothes Washer", img: "{{ URL::asset('img/app_CLOTHS WASHER.jpg') }}"},
-                            {id: 9, pid: 1, Name: "Toilet", img: "{{ URL::asset('img/toilet.png') }}"},
-                            {id: 10, pid: 1, Name: "Composting Toilet", img: "{{ URL::asset('img/toilet.png') }}"},
-                            {id: 11, pid: 1, Name: "Urinal", img: "{{ URL::asset('img/app_LAVATORY.jpg') }}"}
-                        ]
-                    });
-
-                    function preview(){
-
-                        OrgChart.pdfPrevUI.show(chart, {
-                            format: 'A4'
+                            nodeMenu:{
+                                pdf: { text: "Export PDF" },
+                                png: { text: "Export PNG" },
+                                svg: { text: "Export SVG" },
+                                csv: { text: "Export CSV" }
+                            },
+                            toolbar: {
+                                zoom: true,
+                                fit: true,
+                                expandAll: true
+                            },
+                            nodeBinding: {
+                                field_0: "Name",
+                                img_0: "img"
+                            },
+                            nodes: [
+                                {id: 1, Name: "Condensate", img: "data:image/jpeg;base64," + string_icons[0]},
+                                {id: 2, pid: 1, Name: "Kitchen Sink", img: "data:image/jpeg;base64," + string_icons[1]},
+                                {id: 3, pid: 1, Name: "Kitchen Sink + Disposer", img: "data:image/jpeg;base64," + string_icons[2]},
+                                {id: 4, pid: 1, Name: "Dishwasher", img: "data:image/jpeg;base64," + string_icons[3]},
+                                {id: 5, pid: 1, Name: "Lavatory", img: "data:image/jpeg;base64," + string_icons[4]},
+                                {id: 6, pid: 1, Name: "Tub + Shower", img: "data:image/jpeg;base64," + string_icons[5]},
+                                {id: 7, pid: 1, Name: "Fire Suppression", img: "data:image/jpeg;base64," + string_icons[6]},
+                                {id: 8, pid: 1, Name: "Clothes Washer", img: "data:image/jpeg;base64," + string_icons[7]},
+                                {id: 9, pid: 1, Name: "Toilet", img: "data:image/jpeg;base64," + string_icons[8]},
+                                {id: 10, pid: 1, Name: "Composting Toilet", img: "data:image/jpeg;base64," + string_icons[9]},
+                                {id: 11, pid: 1, Name: "Urinal", img: "data:image/jpeg;base64," + string_icons[10]}
+                            ]
                         });
+
+                        function preview(){
+
+                            OrgChart.pdfPrevUI.show(chart, {
+                                format: 'A4'
+                            });
+                        }
                     }
 
                 </script>
