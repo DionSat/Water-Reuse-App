de6c8568c3b5b4f646ed70708f1863afbbad7892
Robin Gearn <robin@wescottdesign.com>
2020-11-02 14:43:54 -0800

    Fix top button size in database/(cities, counties, and state) pages

--- resources/views/database/counties.blade.php
+++ resources/views/database/counties.blade.php
@@ -3,10 +3,10 @@
 @section('body')
     <div class="container">
         <div class="row my-3 d-flex justify-content-between">
-            <div class="col-md-2">
+            <div class="col-md-3">
                 <a href="{{route("admin")}}" class="btn btn-primary d-block"> <i class="fas fa-arrow-circle-left"></i> Dashboard </a>
             </div>
-            <div class="col-md-2">
+            <div class="col-md-3">
                 <a href="{{route("countyAdd")}}" class="btn btn-success d-block"> <i class="fas fa-plus-square"></i> Add New </a>
             </div>
         </div>
