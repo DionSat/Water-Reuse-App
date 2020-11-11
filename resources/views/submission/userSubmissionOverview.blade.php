854b68e740b80900d3c9605a0b85dcc65d262c59
Robin Gearn <robin@wescottdesign.com>
2020-11-11 15:18:50 -0800

    make table in submission overview responsive

--- resources/views/submission/userSubmissionOverview.blade.php
+++ resources/views/submission/userSubmissionOverview.blade.php
@@ -1,40 +1,44 @@
 @extends('layouts.master')
 
 @section('body')
-    <div class="container">
-        <h3 class="text-center">Heres what you submitted:</h3>
-        <table class="table mt-4 mx-auto">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Location</th>
-                    <th scope="col">Source</th>
-                    <th scope="col">Destination</th>
-                    <th scope="col">Time Submitted</th>
-                    <th scope="col">View</th>
-                    <th scope="col">Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($submissions as $submission)
-                    <tr>
-                        <th scope="row">{{$loop->index+1}}</th>
-                        <td>{{$submission->getLocationAsString()}}</td>
-                        <td>{{$submission->source->node_name}}</td>
-                        <td>{{$submission->destination->node_name}}</td>
-                        <td>{{$submission->getTimeSubmittedAsString()}}</td>
-                        <td>
-                            <a href="{{route('viewSubmission', ["type" => $submission->getLocationType(), "state" => $submission->getStatus(), "itemId" => $submission->id, "back" => url()->current()])}}" class="btn btn-primary"> View </a>
-                        </td>
-                        <td>{!! $submission->getStatusAsBadge() !!}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <div class="row mt-4">
+  <div class="container">
+    <h3 class="text-center">Heres what you submitted:</h3>
+    <div class="table-responsive">
+      <table class="table mt-4 mx-auto">
+        <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Location</th>
+          <th scope="col">Source</th>
+          <th scope="col">Destination</th>
+          <th scope="col">Time Submitted</th>
+          <th scope="col">View</th>
+          <th scope="col">Status</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($submissions as $submission)
+          <tr>
+            <th scope="row">{{$loop->index+1}}</th>
+            <td>{{$submission->getLocationAsString()}}</td>
+            <td>{{$submission->source->node_name}}</td>
+            <td>{{$submission->destination->node_name}}</td>
+            <td>{{$submission->getTimeSubmittedAsString()}}</td>
+            <td>
+              <a
+                href="{{route('viewSubmission', ["type" => $submission->getLocationType(), "state" => $submission->getStatus(), "itemId" => $submission->id, "back" => url()->current()])}}"
+                class="btn btn-primary"> View </a>
+            </td>
+            <td>{!! $submission->getStatusAsBadge() !!}</td>
+          </tr>
+        @endforeach
+        </tbody>
+      </table>
+    </div>
+    <div class="row mt-4">
             <span class="mx-auto">
                 {{ $submissions->links() }}
             </span>
-        </div>
     </div>
+  </div>
 @endsection
