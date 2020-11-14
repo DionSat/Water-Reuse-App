c7f8ac2df87d03551f8209ed6df1d094ff775da6
Jonathan Bral <jonbral@pdx.edu>
2020-11-13 20:15:57 -0800

    Changed link colors on Admin pages

--- resources/views/info.blade.php
+++ resources/views/info.blade.php
@@ -135,36 +135,36 @@
                         <div class="container" id="adminContainer">
                             <ul>
                                 <li>
-                                    <a href="#userManip">User Actions</a>
+                                    <a class="text-danger" href="#userManip">User Actions</a>
                                     <ul>
                                         <li>
-                                            <a href="#addAdmin">Add / Remove Administrator Privileges</a>
+                                            <a class="text-danger" href="#addAdmin">Add / Remove Administrator Privileges</a>
                                         </li>
                                         <li>
-                                            <a href="#emailUsers">Send Emails to Users</a>
+                                            <a class="text-danger" href="#emailUsers">Send Emails to Users</a>
                                         </li>
                                         <li>
-                                            <a href="#emailAdmin">Schedule an Administrative Summary Email</a>
+                                            <a class="text-danger" href="#emailAdmin">Schedule an Administrative Summary Email</a>
                                         </li>
                                         <li>
-                                            <a href="#banUser">Ban and Unban a User</a>
+                                            <a class="text-danger" href="#banUser">Ban and Unban a User</a>
                                         </li>
                                     </ul>
                                 </li>
                                 <li>
-                                    <a href="#regulation">Approve or Deny a New Regulation</a>
+                                    <a class="text-danger" href="#regulation">Approve or Deny a New Regulation</a>
                                 </li>
                                 <li>
-                                    <a href="#deleteReg">Delete / Edit a Regulation</a>
+                                    <a class="text-danger" href="#deleteReg">Delete / Edit a Regulation</a>
                                 </li>
                                 <li>
-                                    <a href="#deleteArea">Delete / Edit a State, County or City</a>
+                                    <a class="text-danger" href="#deleteArea">Delete / Edit a State, County or City</a>
                                 </li>
                             </ul>
                             <hr id="userManip">
                             <h2>User Actions</h2>
                                 <h4 id="addAdmin">Add / Remove Administrator Privileges</h4>
-                                    <p>1. Navigate to the <a href="{{ route('admin') }}" target="_blank">'Administrator Dashboard' </a>page.</p>
+                                    <p>1. Navigate to the <a class="text-danger" href="{{ route('admin') }}" target="_blank">'Administrator Dashboard' </a>page.</p>
                                     <p>2. From here, click on the 'view' button under 'All Users'.</p>
                                     <img src="img/banExample.png">
                                     <p>3. Now you can either scroll through the list / pages of users, you could search the current page of users with the 'Search Current Page' option or you can narrow down the search by searching the entire database of users with the 'Search Database' option</p>
@@ -174,31 +174,31 @@
                                     <p>5. You should now see either a green check mark or a red 'x', the former meaning the user has administrator privileges, and the later meaning they do not.</p>
                                     <img class="smallRec" src="img/toggleAdminExample1.png">
                                     <br>
-                                    <a href="#nav-tab-admin">Back to Top</a>
+                                    <a class="text-danger" href="#nav-tab-admin">Back to Top</a>
                                 <hr id="emailUsers">
                                 <h4 style="margin-top: 1em;">Email Users</h4>
-                                    <p>1. Navigate to the <a href="{{ route('admin') }}" target="_blank">'Administrator Dashboard' </a>page.</p>
+                                    <p>1. Navigate to the <a class="text-danger" href="{{ route('admin') }}" target="_blank">'Administrator Dashboard' </a>page.</p>
                                     <p>2. From here, click on the 'view' button under 'All User Emails'.</p>
                                     <img src="img/userEmailExample.png"></img>
                                     <p>3. You will now see that there are two options. You can either email consenting users (preferred), or you can email all users. Generally, you only want to email non-consenting users if it is an absolute emergency.</p>
                                     <img src="img/userEmailExample1.png"></img>
                                     <p>4. Once you have chosen which user base to email, your default email software will open up on your computer prefilled with a list of user emails. If you do not have email capable software installed, we suggest to install one to use this functionality.</p>
                                     <br>
-                                    <a href="#nav-tab-admin">Back to Top</a>
+                                    <a class="text-danger" href="#nav-tab-admin">Back to Top</a>
                                 <hr id="emailAdmin">
                                 <h4 style="margin-top: 1em;">Schedule an Administrative Summary Email</h4>
                                     <p>This will send an email with a summary of all pending administrator duties, such as approving a regulation.</p>
-                                    <p>1. Navigate to the <a href="{{ route('admin') }}" target="_blank">'Administrator Dashboard' </a>page.</p>
+                                    <p>1. Navigate to the <a class="text-danger" href="{{ route('admin') }}" target="_blank">'Administrator Dashboard' </a>page.</p>
                                     <p>2. From here, click on the 'view' button under 'Scheduled Emails'.</p>
                                     <img src="img/emailAdminExample.png"></img>
                                     <p>3. You may now select which administrator to email under the 'Admin User' dropdown. Your username will be selected by default. You can also select how often you would want your email sent in the 'How often (in days)' edit box.</p>
                                     <img class="smallRec" src="img/emailAdminExample1.png"></img>
                                     <p>4. Once you click the 'Schedule' button, an email containing administrative information will be sent at 10am every number of days specified by you.</p>
                                     </br>
-                                    <a href="#nav-tab-admin">Back to Top</a>
+                                    <a class="text-danger" href="#nav-tab-admin">Back to Top</a>
                                 <hr id="banUser">
                                 <h4 style="margin-top: 1em;">Ban and Unban a User</h4>
-                                    <p>1. Navigate to the <a href="{{ route('admin') }}" target="_blank">'Administrator Dashboard' </a>page.</p>
+                                    <p>1. Navigate to the <a class="text-danger" href="{{ route('admin') }}" target="_blank">'Administrator Dashboard' </a>page.</p>
                                     <p>2. From here, click on the 'view' button under 'All Users'.</p>
                                     <img src="img/banExample.png"></img>
                                     <p>3. Now you can either scroll through the list / pages of users, you could search the current page of users with the 'Search Current Page' option or you can narrow down the search by searching the entire database of users with the 'Search Database' option</p>
@@ -211,10 +211,10 @@
                                     <img src="img/banExample4.png"></img>
                                     <img class="smallRec" src="img/banExample5.png"></img>
                                     </br>
-                                    <a href="#nav-tab-admin">Back to Top</a>
+                                    <a class="text-danger" href="#nav-tab-admin">Back to Top</a>
                             <hr id="regulation">
                             <h2>Approve or Deny a New Regulation</h2>
-                                <p>1. Navigate to the <a href="{{ route('adminUserSubmissionView') }}" target="_blank">'Submissions' </a>page.</p>
+                                <p>1. Navigate to the <a class="text-danger" href="{{ route('adminUserSubmissionView') }}" target="_blank">'Submissions' </a>page.</p>
                                 <p>2. From here you should see three options showing the count of pending State, County and City regulation submissions. Choose the area level you wish to approve for. For this example, we will be approving a State level submission, but these steps apply to County and City as well.</p>
                                 <img src="img/regulationExample.png"></img>
                                 <p>3. You should now see a list of regulations awaiting approval. To review the submission, click the 'view' button, to decline the submission click the 'Decline' button and to approve the submission, click the 'Approve' button.</p>
@@ -223,10 +223,10 @@
                                 <p>The system is set up in a way that if you approve a new or non-existing area, this area will be created.</p>
                                 </br>
                                 </br>
-                                <a href="#nav-tab-admin">Back to Top</a>
+                                <a class="text-danger" href="#nav-tab-admin">Back to Top</a>
                             <hr id="deleteReg">
                             <h2>Delete / Edit a Regulation</h2>
-                                <p>1. Navigate to the <a href="{{ route('search') }}" target="_blank"> Search </a>page</p>
+                                <p>1. Navigate to the <a class="text-danger" href="{{ route('search') }}" target="_blank"> Search </a>page</p>
                                 <p>2. Select either 'Commercial' or 'Residential'</p>
                                 <img src="img/searchExample.png"></img>
                                 <p>3. First select a state in the dropdown menu, then you can either search only by state, or by county or city by selecting them in the dropdown menu.</p>
@@ -239,10 +239,10 @@
                                 <p>7. From this page, you can either delete or edit a regulation by clicking the associated buttons.</p>
                                 <img src="img/deleteRegExample.png"></img>
                                 </br>
-                                <a href="#nav-tab-admin">Back to Top</a>
+                                <a class="text-danger" href="#nav-tab-admin">Back to Top</a>
                             <hr id="deleteArea">
                             <h2>Delete a State, County or City</h2>
-                                <p>1. Navigate to the <a href="{{ route('admin') }}" target="_blank">'Administrator Dashboard' </a>page.</p>
+                                <p>1. Navigate to the <a class="text-danger" href="{{ route('admin') }}" target="_blank">'Administrator Dashboard' </a>page.</p>
                                 <p>2. Click on the 'Database' button to switch to the database view.</p>
                                 <img src="img/deleteExample.png"></img>
                                 <p>3. You should now see display boxes for Cities, Counties and States. If you wish to add a new area, click '+Add', but if you wish to edit or delete an area, click 'Manage'</p>
@@ -250,7 +250,7 @@
                                 <p>4. From the 'Manage' screen, you will see button options to either 'Delete', if you wish to remove an area, or 'Modify', if you just need to edit the area.</p>
                                 <img src="img/deleteAreaExample1.png"></img>
                                 </br>
-                                <a href="#nav-tab-admin">Back to Top</a>
+                                <a class="text-danger" href="#nav-tab-admin">Back to Top</a>
                         </div>
                 </div>
             </div>
