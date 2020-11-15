4e9528637a9d91bd102b030f408bd4a5198275b6
LiamRotchford5671 <liamrotchford@gmail.com>
2020-11-15 10:57:52 -0800

    CapD-44: Adjusted scheduled emails page from admin>dahsboard>scheduledEmails

--- resources/views/admin/viewScheduledEmail.blade.php
+++ resources/views/admin/viewScheduledEmail.blade.php
@@ -3,14 +3,14 @@
 @section('body')
   <div class="container">
     <a href="{{route("admin")}}" class="btn btn-primary col-md-2"> <i class="fas fa-arrow-circle-left"></i> Back </a>
-    <h3 class="text-center mt-5">Scheduled Emails</h3>
-    <div class="text-muted text-center"> The users listed below will receive a summary email however often they select.
+    <h3 class="text-center text-light mt-5">Scheduled Emails</h3>
+    <div class="text-light text-center"> The users listed below will receive a summary email however often they select.
       Summary emails are sent out at 10am.
     </div>
     <hr>
     <div class="row">
       <div class="col-md-10 mx-auto">
-        <table id="userTable" class="table">
+        <table id="userTable" class="table text-light">
           <thead>
           <tr>
             <th scope="col" class="text-center">User Id</th>
@@ -40,7 +40,7 @@
           </tbody>
         </table>
 
-        <h5 class="text-center mt-5">
+        <h5 class="text-center text-light mt-5">
           Schedule Another Summary Email
         </h5>
         <hr>
@@ -49,7 +49,7 @@
           <div class="row">
             <div class="container col-md-5">
               <div class="form-group row">
-                <label class="col-form-label col-md text-md-right" for="user_id"> Admin User </label>
+                <label class="col-form-label col-md text-md-right text-light" for="user_id"> Admin User </label>
                 <div class="col-md pl-md-0">
                   <select name="user_id" class="form-control">
                     @foreach($adminUsers as $admin)
@@ -62,7 +62,7 @@
             </div>
             <div class="container col-md-7">
               <div class="form-group row">
-                <label class="col-form-label col-md-6 text-md-right" for="email_frequency">
+                <label class="col-form-label col-md-6 text-md-right text-light" for="email_frequency">
                   How Often (in days)
                 </label>
                 <div class="col-md-6 pl-md-0">
@@ -85,4 +85,26 @@
     </div>
   </div>
 @endsection
+
+@push("css")
+  <style>
+
+    body::before {
+      content: "";
+      position: fixed;
+      width: 200%;
+      height: 200%;
+      top: -50%;
+      left: -50%;
+      z-index: -1;
+      background-size: cover;
+      background-image: url('https\3a //scontent.fhio2-1.fna.fbcdn.net/v/t15.5256-10/cp0/e15/q65/p600x600/20505009_401019313628316_6647662209365180416_n.jpg?_nc_cat\3d 102\26 ccb\3d 2\26 _nc_sid\3d ccf8b3\26 _nc_ohc\3d XdbCgFERuygAX-1bAXs\26 _nc_ht\3d scontent.fhio2-1.fna\26 oh\3d 11dfc9e7c7ba03c61b70c4b3b4f3bd0c\26 oe\3d 5FD1716B');
+      no-repeat fixed center center;
+      background-size: cover;
+      transform: scaleY(-1);
+      filter: brightness(80%);
+    }
+  </style>
+@endpush
+
 
