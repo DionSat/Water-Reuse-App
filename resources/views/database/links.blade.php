869f82b0d90fee7517716897cf081b5eb53a3b00
LiamRotchford5671 <liamrotchford@gmail.com>
2020-11-16 13:53:03 -0800

    CapD-44: Updated all subpages that wouln't have master layout styling applied to all pull the same css asset with one css set for the background image.

--- resources/views/database/links.blade.php
+++ resources/views/database/links.blade.php
@@ -1,7 +1,8 @@
 @extends('layouts.master')
 
 @section('body')
-    <div class="container-fluid">
+  <link href=" {{ URL::asset('css/backgroundStyle.css') }}" rel="stylesheet">
+  <div class="container-fluid">
         <div class="row my-3 d-flex justify-content-between">
             <div class="col-md-3">
                 <a href="{{route("admin")}}" class="btn btn-primary d-block"> <i class="fas fa-arrow-circle-left"></i> Dashboard </a>
@@ -75,33 +76,3 @@
     </div>
 
 @endsection
-
-@push('css')
-    <style>
-        /*
-        .table th {
-            text-align: center;
-        }
-
-        .table td {
-            text-align: center;
-        }
-
-         */
-        body::before {
-          content: "";
-          position: fixed;
-          width: 200%;
-          height: 200%;
-          top: -50%;
-          left: -50%;
-          z-index: -1;
-          background-size: cover;
-          background-image: url('https\3a //scontent.fhio2-1.fna.fbcdn.net/v/t15.5256-10/cp0/e15/q65/p600x600/20505009_401019313628316_6647662209365180416_n.jpg?_nc_cat\3d 102\26 ccb\3d 2\26 _nc_sid\3d ccf8b3\26 _nc_ohc\3d XdbCgFERuygAX-1bAXs\26 _nc_ht\3d scontent.fhio2-1.fna\26 oh\3d 11dfc9e7c7ba03c61b70c4b3b4f3bd0c\26 oe\3d 5FD1716B');
-          no-repeat fixed center center;
-          transform: scaleY(-1);
-          filter: brightness(80%);
-        }
-    </style>
-
-@endpush
